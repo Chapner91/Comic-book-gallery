46c4097c6f29dbd3909092a2cc8d001bb38babfb
Jason Hapner <chapner@gmail.com>
2022-06-22 11:48:08 -0400

    added a project to practice EF migration techniques

---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Migrations/202206221522529_AddComicBookAverageRatingEntity.cs
@@ -0,0 +1,57 @@
+﻿namespace ComicBookGalleryModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddComicBookAverageRatingEntity : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ComicBookAverageRating",
+                c => new
+                    {
+                        ComicBookAverageRatingID = c.Int(nullable: false, identity: true),
+                        ComicBookId = c.Int(nullable: false),
+                        AverageRating = c.Decimal(nullable: false, precision: 5, scale: 2),
+                        Date = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ComicBookAverageRatingID)
+                .ForeignKey("dbo.ComicBook", t => t.ComicBookId, cascadeDelete: true)
+                .Index(t => t.ComicBookId);
+
+			// Populate the initial values for the ComicBookAverageRating table
+			Sql(@"
+				INSERT INTO dbo.ComicBookAverageRating 
+					(ComicBookId, AverageRating, Date) 
+				SELECT 
+					Id, AverageRating, GETDATE() 
+				FROM dbo.ComicBook 
+				WHERE AverageRating IS NOT NULL");
+
+            DropColumn("dbo.ComicBook", "AverageRating");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.ComicBook", "AverageRating", c => c.Decimal(precision: 5, scale: 2));
+
+			// Populate ComicBook.AverageRating column with most recent average rating value
+			Sql(@"
+				update cb
+				set cb.AverageRating = cbar.AverageRating
+				from ComicBook cb
+				cross apply (
+					select top 1 AverageRating, Date
+					from ComicBookAverageRating 
+					where ComicBookId = cb.Id
+					order by Date desc
+				) as cbar
+			");
+
+            DropForeignKey("dbo.ComicBookAverageRating", "ComicBookId", "dbo.ComicBook");
+            DropIndex("dbo.ComicBookAverageRating", new[] { "ComicBookId" });
+            DropTable("dbo.ComicBookAverageRating");
+        }
+    }
+}
---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBookAverageRating.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel.Models
+{
+	public class ComicBookAverageRating
+	{
+		//DB Properties
+		public int ComicBookAverageRatingID { get; set; }
+		public int ComicBookId { get; set; }
+		public decimal AverageRating { get; set; }
+		public DateTime Date { get; set; }
+
+		//Navigation Properties
+		public ComicBook ComicBook { get; set; }
+	}
+}
